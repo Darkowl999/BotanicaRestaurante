d1e2d362c30fda0ab95cd74835c938c1dfbbe76e
Darkowl999 <raulalbertoortiz290199@hotmail.com>
2021-07-21 17:51:47 -0400

    añadiendo ficheros y archivos

--- src/index.js
+++ src/index.js
@@ -1,9 +1,33 @@
 const express= require('express');
-
+const path= require('path');
+const exphbs=require('express-handlebars');
+const methodOverride=require('method-override');
 const app=express();
 
 
+
 const port= process.env.PORT||4000;
+
+
+//settings 
+app.set('port',process.env.PORT||4000);
+app.set('views',path.join(__dirname,'views'));
+app.engine('.hbs',exphbs({
+    defaultLayout:'main',
+    layoutsDir:path.join(app.get('views'),'layouts'),
+   // partialsDir:path.join(app.get('vies'),'partials'),
+    extname:'.hbs'
+}));
+app.set('view engine','hbs');
+
+//static files 
+app.use(express.static(path.join(__dirname,'public')));
+
+
+//routes 
+app.use(require('./routes/index'));
+
+
 
 
 
